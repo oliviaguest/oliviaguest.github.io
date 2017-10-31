997ec99f92091873016f47b323629bc448c3ff2b
Olivia Guest <guest.olivia@gmail.com>
2017-10-31 12:19:14 +0000

    tidy

--- js/accordion.js
+++ js/accordion.js
@@ -1,26 +1,28 @@
 $(function() {
-
-    $("#accordion").accordion({
-      heightStyle: "content",
-      collapsible: true,
-      active: false,
-      header: "h1"
-
-    });
-    // get the #section from the URL
-    var hash = window.location.hash;
-    hash = hash.slice(1);
-    console.log(hash);
-    var active = false;
-    if (hash == 'publications' | hash == 'pubs' | hash == 'pub') {
-      active = 0;
-    }
-    else if (hash == 'cv' | hash == 'curriculum_vitae' | hash == 'vitae') {
-      active = 1;
-    }
-    if (hash) {
-      $( "#accordion" ).accordion( "option", "active", active );
-      $('html,body').animate({scrollTop: $("#accordion").offset().top});
-    }
-
+  // Initialise the accordion:
+  $('#accordion').accordion({
+    heightStyle: 'content',
+    collapsible: true,
+    active: false,
+    header: 'h1'
   });
+  // Get the #hash in the URL:
+  var hash = window.location.hash;
+  hash = hash.slice(1);
+  // console.log(hash);
+  var active = false;
+  // Activate appropriate accordion item:
+  if (hash === 'publications' | hash === 'pubs' | hash === 'pub') {
+    active = 0;
+  } else if (hash === 'cv' | hash === 'curriculum_vitae' | hash === 'vitae') {
+    active = 1;
+  }
+  if (hash) {
+    $('#accordion').accordion('option', 'active', active);
+    $('html,body').animate({
+      scrollTop: $('#accordion').offset().top
+    });
+  }
+
+
+});
--- js/fix_altmetric.js
+++ js/fix_altmetric.js
@@ -1,13 +1,19 @@
+function fixAltmetric() {
+  var $width = $(window).width();
+
+  if ($width < (550 + 280)) {
+    $('.altmetric-embed').attr('data-badge-popover', 'left');
+  } else {
+    $('.altmetric-embed').attr('data-badge-popover', 'right');
+  }
+};
+
 $(document).ready(function() {
-      fixAltmetric();
+  fixAltmetric();
 });
 
-function fixAltmetric() {
-      var $width = $(window).width();
 
-      if ($width < 550+280) {
-            $(".altmetric-embed").attr("data-badge-popover", "left");
-      } else {
-            $(".altmetric-embed").attr("data-badge-popover", "right");
-      }
-};
+// This doesn't work as one might expect:
+// $(window).on('resize', function () {
+//   fixAltmetric();
+// });
