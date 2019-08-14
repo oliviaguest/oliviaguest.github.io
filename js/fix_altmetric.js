0a5c456620b307c80e6f321a27f7e1cd6fa9d6a0
oliviaguest <guest.olivia@gmail.com>
2019-08-14 20:23:46 +0300

    new layout

--- js/fix_altmetric.js
+++ js/fix_altmetric.js
@@ -1,19 +1,13 @@
 function fixAltmetric() {
   var $width = $(window).width();
 
-  if ($width < (550 + 280)) {
+  // if ($width < (550 + 280)) {
     $('.altmetric-embed').attr('data-badge-popover', 'left');
-  } else {
-    $('.altmetric-embed').attr('data-badge-popover', 'right');
-  }
+  // } else {
+  //   $('.altmetric-embed').attr('data-badge-popover', 'right');
+  // }
 };
 
 $(document).ready(function() {
   fixAltmetric();
 });
-
-
-// This doesn't work as one might expect:
-// $(window).on('resize', function () {
-//   fixAltmetric();
-// });
